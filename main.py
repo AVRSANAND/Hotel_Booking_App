8a27484019163af21fee6bdf591fa7598115c9c3
AVRSANAND <avrsanand2003@gmail.com>
2024-07-13 11:40:38 +0530

    Available & book methods implemented

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 import pandas
 
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
 
-df = pandas.read_csv("hotels.csv")
 
 class Hotel:
-    def __init__(self, id):
-        pass
-    def book(self):
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
         pass
 
+    def book(self):
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
     def available(self):
-        pass
+        """Check if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 
 class ReservationTicket:
 
-    def __init__(self, customer_name, hotel):
+    def __init__(self, customer_name, hotel_object):
         pass
+
     def generate(self):
         pass
 
 
 print(df)
-id = input("Enter the id of the Hotel")
-hotel = Hotel(id)
+hotel_id = input("Enter the id of the Hotel: ")
+hotel = Hotel(hotel_id)
 if hotel.available():
     hotel.book()
-    name = input("Enter your name")
+    name = input("Enter your name: ")
     reservation_ticket = ReservationTicket(name, hotel)
     print(reservation_ticket.generate())
 else:
