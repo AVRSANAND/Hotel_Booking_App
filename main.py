8a1b6352fef090ae0df02d110cd26409ab195c24
AVRSANAND <avrsanand2003@gmail.com>
2024-07-19 23:22:56 +0530

    Create CreditCard System

--- main.py
+++ main.py
@@ -37,13 +37,25 @@ class ReservationTicket:
         """
         return content
 
+class CreditCard:
+    def __init__(self, number):
+
+        pass
+
+    def validate(self, expiration, holder, cvv):
+        pass
+
 print(df)
 hotel_id = input("Enter the id of the Hotel: ")
 hotel = Hotel(hotel_id)
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+    credit_card = CreditCard(number="1234567890123456")
+    if credit_card.validate(expiration="12/26", holder="Anand", cvv="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("There was a problem with your payment")
 else:
     print("Hotel is not available to book")
