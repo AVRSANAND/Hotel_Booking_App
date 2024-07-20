4a352595830fb76a43e92b3b29fed1c0a60bbf2b
AVRSANAND <avrsanand2003@gmail.com>
2024-07-20 22:38:40 +0530

    Validating CreditCard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -37,20 +38,27 @@ class ReservationTicket:
         """
         return content
 
+
 class CreditCard:
     def __init__(self, number):
-
+        self.number = number
         pass
 
     def validate(self, expiration, holder, cvv):
-        pass
+        card_data = {"number":self.number, "expiration":expiration,
+                     "holder":holder,"cvv":cvv}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
 
 print(df)
 hotel_id = input("Enter the id of the Hotel: ")
 hotel = Hotel(hotel_id)
 if hotel.available():
     credit_card = CreditCard(number="1234567890123456")
-    if credit_card.validate(expiration="12/26", holder="Anand", cvv="123"):
+    if credit_card.validate(expiration="12/26", holder="AVRS ANAND", cvv="123"):
         hotel.book()
         name = input("Enter your name: ")
         reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
